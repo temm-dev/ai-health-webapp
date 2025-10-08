fd3391384bce34465e74ee1272a975dabdf64a99
temm-dev <temm-dev@proton.me>
2025-10-08 22:38:23 +0300

    Minor edits

--- src/app/services/face_analysis.py
+++ src/app/services/face_analysis.py
@@ -1,44 +1,39 @@
 import os
 import time
-from typing import Any, Dict, List
 
 from deepface import DeepFace
 
+
 class FaceAnalysis:
 
-    async def base_analyze(self, image_path: str
-    ) -> Dict[str, Any] | List[Dict[str, Any]] | None:
-        """"""
+    async def analyze(
+        self, image_path: str, actions=("age", "gender", "race", "emotion")
+    ):
         path = f"data/temp/{image_path}"
 
         if os.path.exists(path):
             t1 = time.time()
 
-            objs = DeepFace.analyze(
-                img_path=path, actions=['age', 'gender', 'race', 'emotion']
-            )
+            objs = DeepFace.analyze(img_path=path, actions=actions)
 
-            objs[0]["time"] = time.time() - t1 # type: ignore
+            objs[0]["time"] = time.time() - t1  # type: ignore
 
             return objs[0]
 
-        return None
-    
+        return {}
+
     async def anti_spoofing_test(self, image_path: str):
         path = f"data/temp/{image_path}"
 
         if os.path.exists(path):
             t1 = time.time()
-            face_objs = DeepFace.extract_faces(img_path=path, anti_spoofing = True)
+            face_objs = DeepFace.extract_faces(img_path=path, anti_spoofing=True)
 
             objs = {
-                "confidence": face_objs[0]["confidence"],
+                "antispoof_score": face_objs[0]["antispoof_score"],
                 "is_real": face_objs[0]["is_real"],
-                "antispoof_score": face_objs[0]["antispoof_score"]
             }
 
-            objs["time"] = time.time() - t1 # type: ignore
+            objs["time"] = time.time() - t1  # type: ignore
 
             return objs
-
-
