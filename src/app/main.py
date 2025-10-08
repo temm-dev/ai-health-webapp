06a21d308ecae2e4028e05eaeec1e54c15cb1022
temm-dev <temm-dev@proton.me>
2025-10-08 22:45:42 +0300

    Minor edits

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.api.endpoints import health, upload
+
+from app.api.endpoints import upload
 
 app = FastAPI(title="Health Face Analysis API")
 
-app.include_router(health.router, prefix="/api/v1")
 app.include_router(upload.router, prefix="/api/v1")
 
 
@@ -12,7 +12,7 @@ async def root():
     return {"message": "Health Face Analysis API is running!"}
 
 
-
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
