85958be8757affdc4a41caaa592ab6f3c3baebc1
temm-dev <temm-dev@proton.me>
2025-10-14 20:56:26 +0300

    The code for getting data from a photo has been entered into the image_processing function.

--- src/app/api/endpoints/upload.py
+++ src/app/api/endpoints/upload.py
@@ -5,29 +5,15 @@ from typing import List, Optional
 from fastapi import APIRouter, File, HTTPException, Query, UploadFile
 from fastapi.responses import JSONResponse
 
-
 from app.core.schemas import UploadResponse
-from app.services.calculate_metrics import CalculateMetrics
-from app.services.face_analysis import FaceAnalysis
-from app.services.face_metrics import FaceMetricsAnalysis
-from app.services.skin_analysis import SkinTestAnalysis
 from app.langgraf.langgraf_logic import analyze_from_json
-
-from app.utils.utils import compress_image, convert_numpy_types
-
-
+from app.utils.utils import compress_image, image_processing
 
 router = APIRouter(tags=["upload"])
 
 # Директория для сохранения фото
 UPLOAD_DIR = "data/temp"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
-
-face_analysis = FaceAnalysis()
-skin_test = SkinTestAnalysis()
-face_metrics_analysis = FaceMetricsAnalysis()
-calculate_metrics = CalculateMetrics()
-
 
 
 @router.post("/upload", response_model=UploadResponse)
@@ -47,7 +33,7 @@ async def upload_file(
 
         content = await file.read()
 
-        compressed_content = compress_image(
+        compressed_content = await compress_image(
             content,
             max_size=(1200, 1200),  # Максимальный размер
             quality=75,  # Качество 75%
@@ -64,82 +50,12 @@ async def upload_file(
         else:
             analysis_actions = ("age", "gender", "race", "emotion")
 
-        return_response = {}
+        response = await image_processing(
+            unique_filename, analysis_actions, bool(anti_spoofing_test)
+        )
+        print(response)
 
-        try:
-            face_analysis_data = await face_analysis.analyze(
-                unique_filename, analysis_actions
-            )
-            face_analysis_data = convert_numpy_types(face_analysis_data)
-            return_response.update(face_analysis_data)  # type: ignore
-        except:
-            pass
-
-        try:
-            skin_test_data = await skin_test.analyze(unique_filename)
-            skin_test_data = convert_numpy_types(skin_test_data)
-            return_response.update(skin_test_data)  # type: ignore
-        except:
-            pass
-
-        try:
-            metrics_face_data = face_metrics_analysis.analyze(unique_filename)
-            metrics_face_data = convert_numpy_types(metrics_face_data)
-
-            age = face_analysis_data["age"]  # type: ignore
-            emotions = return_response["emotion"]
-            eyebags = skin_test_data["eyebags"]  # type: ignore
-            average_eye_openness = metrics_face_data["average_eye_openness"]  # type: ignore
-            muscle_tension = metrics_face_data["muscle_tension"]  # type: ignore
-            redness = skin_test_data["redness"]  # type: ignore
-            acne = skin_test_data["acne"]  # type: ignore
-            facial_symmetry = metrics_face_data["facial_symmetry"] # type: ignore
-
-            stress = calculate_metrics.calculate_stress_level(
-                emotions, eyebags, muscle_tension  # type: ignore
-            )
-            sleep_quality = calculate_metrics.calculate_sleep_quality(
-                eyebags, redness, average_eye_openness, age  # type: ignore
-            )
-            calculate_skin_health_index = calculate_metrics.calculate_skin_health_index(
-                acne, redness, age  # type: ignore
-            )
-            vitality_score = calculate_metrics.calculate_vitality_score(
-                {
-                    "stress_level": stress,
-                    "sleep_quality": sleep_quality,
-                    "skin_health_index": calculate_skin_health_index,
-                    "facial_symmetry": facial_symmetry,
-                    "muscle_tension": muscle_tension
-                }  # type: ignore
-            )
-
-            stress = {"stress_level": stress}
-            sleep_quality = {"sleep_quality": sleep_quality}
-            skin_health_index = {"skin_health_index": calculate_skin_health_index}
-            vitality_score = {"vitality_score": vitality_score}
-
-            return_response.update(metrics_face_data)  # type: ignore
-            return_response.update(stress)
-            return_response.update(sleep_quality)
-            return_response.update(skin_health_index)
-            return_response.update(vitality_score)
-        except Exception as e:
-            print(e)
-
-        if anti_spoofing_test:
-            try:
-                anti_spoofing_test_data = await face_analysis.anti_spoofing_test(
-                    unique_filename
-                )
-                return_response.update(anti_spoofing_test_data)  # type: ignore
-            except:
-                pass
-
-        
-        print(return_response)
-        
-        llmresponse = await analyze_from_json(return_response)
+        llmresponse = await analyze_from_json(response)
 
         print(llmresponse)
 
--- src/app/utils/utils.py
+++ src/app/utils/utils.py
@@ -1,13 +1,21 @@
 import io
-# import json
-import numpy as np
 
+import numpy as np
 from PIL import Image
 
+from app.services.calculate_metrics import CalculateMetrics
+from app.services.face_analysis import FaceAnalysis
+from app.services.face_metrics import FaceMetricsAnalysis
+from app.services.skin_analysis import SkinTestAnalysis
+from app.utils.utils import convert_numpy_types
+
+face_analysis = FaceAnalysis()
+skin_test = SkinTestAnalysis()
+face_metrics_analysis = FaceMetricsAnalysis()
+calculate_metrics = CalculateMetrics()
 
 
-def compress_image(image_data, max_size=(800, 800), quality=85):
-    """"""
+async def compress_image(image_data, max_size=(800, 800), quality=85):
     try:
         image = Image.open(io.BytesIO(image_data))
 
@@ -23,7 +31,8 @@ def compress_image(image_data, max_size=(800, 800), quality=85):
     except Exception as e:
         raise Exception(f"Image compression error: {str(e)}")
 
-def convert_numpy_types(obj):
+
+async def convert_numpy_types(obj) -> dict | list | tuple | float | bool:
     if isinstance(obj, dict):
         return {key: convert_numpy_types(value) for key, value in obj.items()}
     elif isinstance(obj, list):
@@ -41,6 +50,64 @@ def convert_numpy_types(obj):
     else:
         return obj
 
-# def safe_json_serialize(obj):
-#     """Безопасная сериализация в JSON с конвертацией numpy типов"""
-#     return json.loads(json.dumps(obj, default=lambda x: convert_numpy_types(x)))
+
+async def image_processing(
+    filename: str, analysis_actions: tuple, anti_spoofing_test: bool
+):
+    response = {}
+
+    face_data = await face_analysis.analyze(filename, analysis_actions)
+    skin_data = await skin_test.analyze(filename)
+    metrics_data = await face_metrics_analysis.analyze(filename)
+
+    face_data = await convert_numpy_types(face_data)
+    skin_data = await convert_numpy_types(skin_data)
+    metrics_data = await convert_numpy_types(metrics_data)
+
+    response.update(face_data)  # type: ignore
+    response.update(skin_data)  # type: ignore
+
+    age = face_data["age"]  # type: ignore
+    emotions = response["emotion"]
+    eyebags = skin_test_data["eyebags"]  # type: ignore
+    average_eye_openness = metrics_face_data["average_eye_openness"]  # type: ignore
+    muscle_tension = metrics_face_data["muscle_tension"]  # type: ignore
+    redness = skin_test_data["redness"]  # type: ignore
+    acne = skin_test_data["acne"]  # type: ignore
+    facial_symmetry = metrics_face_data["facial_symmetry"]  # type: ignore
+
+    stress = await calculate_metrics.calculate_stress_level(
+        emotions, eyebags, muscle_tension
+    )
+    sleep_quality = await calculate_metrics.calculate_sleep_quality(
+        eyebags, redness, average_eye_openness, age
+    )
+    skin_health_index = await calculate_metrics.calculate_skin_health_index(
+        acne, redness, age
+    )
+    vitality_score = await calculate_metrics.calculate_vitality_score(
+        {
+            "stress_level": stress,
+            "sleep_quality": sleep_quality,
+            "skin_health_index": skin_health_index,
+            "facial_symmetry": facial_symmetry,
+            "muscle_tension": muscle_tension,
+        }
+    )
+
+    stress = {"stress_level": stress}
+    sleep_quality = {"sleep_quality": sleep_quality}
+    skin_health_index = {"skin_health_index": skin_health_index}
+    vitality_score = {"vitality_score": vitality_score}
+
+    response.update(metrics_face_data)  # type: ignore
+    response.update(stress)
+    response.update(sleep_quality)
+    response.update(skin_health_index)
+    response.update(vitality_score)
+
+    if anti_spoofing_test:
+        anti_spoofing_test_data = await face_analysis.anti_spoofing_test(filename)
+        response.update(anti_spoofing_test_data)  # type: ignore
+
+    return response
